63c02f83fb0c5e8e9473a9a0bcd39d61ec297b69
yujinkim1 <yujin7621@me.com>
2023-06-18 22:10:05 +0900

    modified(181939): From merge larger to 더 크게 합치기

--- SwiftKick/ProgrammersBeginner/더_크게_합치기.swift
+++ SwiftKick/ProgrammersBeginner/더_크게_합치기.swift
@@ -1,12 +1,11 @@
 //
-//	MergeLarger.swift
-//	SwiftKick
+//  더_크게_합치기.swift
+//  SwiftKick
 //
-//	Created by Yujin Kim on 2023-06-11.
+//  Created by Yujin Kim on 2023-06-11.
 //
 /**
  * https://school.programmers.co.kr/learn/courses/30/lessons/181939
- * 더 크게 합치기
  *
  * 두 정수에 대한 연산으로 두 정수 [a]와 [b]를 붙여서 쓴 값을 반환한다.
  * 만약 [a]와 [b]가 같다면 a + b를 반환하고, 서로 다르면 둘 중 더 큰 값으로 반환한다.
