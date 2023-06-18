6754cc99f71dd1ea6ea103e55e4199beb7658a08
yujinkim1 <yujin7621@me.com>
2023-06-18 22:12:03 +0900

    modified(181940): From multiple string to 문자열 곱하기

--- SwiftKick/ProgrammersBeginner/문자열_곱하기.swift
+++ SwiftKick/ProgrammersBeginner/문자열_곱하기.swift
@@ -1,12 +1,11 @@
 //
-//	MultiplyString.swift
-//	SwiftKick
+//  문자열_곱하기.swift
+//  SwiftKick
 //
-//	Created by Yujin Kim on 2023-06-11.
+//  Created by Yujin Kim on 2023-06-11.
 //
 /**
  * https://school.programmers.co.kr/learn/courses/30/lessons/181940
- * 문자열 곱하기
  *
  * 문자열 [my_string]과 정수 [k]가 주어질 때, [my_string]을 [k]번 반복한 문자열을 반환하는 함수를 작성
  * [my_string] 변수가 "string" 이고 [k] 변수가 3 이라면 result 변수가 "stringstringstring"이다.
