f657fd4ea6f34fefc9671ddec0465ae68f771342
yujinkim1 <yujin7621@me.com>
2023-06-25 20:13:10 +0900

    modified(comment): 문자열 뒤의 n글자 url

--- SwiftKick/ProgrammersBeginner/문자열의_뒤의_n글자.swift
+++ SwiftKick/ProgrammersBeginner/문자열의_뒤의_n글자.swift
@@ -5,6 +5,8 @@
 //  Created by Yujin Kim on 2023-06-25.
 //
 /**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181910
+ *
  * 문자열 [myString]변수와 정수형 [n]변수가 주어진다.
  * [myString]변수에서 [n]변수 만큼의 뒷 글자를 반환한다.
  * input: "ProgrammerS123", 11 / output: "grammerS123"
